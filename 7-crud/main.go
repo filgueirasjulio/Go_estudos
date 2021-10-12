7957bb935a255b69914d5636906b7373cffb8773
Julio Filgueiras <julio_filgueiras@live.com>
2021-10-12 16:01:44 -0300

    feat: conexão com banco de dados

---
+++ 7-crud/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_"github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	stringConexao := "root:1234@/crud_go?charset=utf8&parseTime=True&loc=Local"
+	
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão está aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+}
