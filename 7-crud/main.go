c00516753125ac1dee536b728f9454fcc06d4231
Julio Filgueiras <julio_filgueiras@live.com>
2021-10-12 21:18:04 -0300

    feat(crud): create

--- 7-crud/main.go
+++ 7-crud/main.go
@@ -5,10 +5,13 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"crud/servidor"
 )
 
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods("POST")
 
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
